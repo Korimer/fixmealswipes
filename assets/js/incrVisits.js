6b400b0b0243ff5ca40e11cd2478b014fcbea893
Korimer <thekclov@gmail.com>
2025-04-21 12:34:51 -0600

    restored button and other elements

--- assets/js/incrVisits.js
+++ assets/js/incrVisits.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem("Swipes") == null) {
-        localStorage.setItem("Swipes",2);
-    }
-    else {
-        localStorage.setItem("Swipes", localStorage.getItem("Swipes") - 1);
-    }
-    if (localStorage.getItem("UsingSwipes") == null) {
-        localStorage.setItem("UsingSwipes",true);
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem("Swipes") == null) {
+        localStorage.setItem("Swipes",2);
+    }
+    else {
+        localStorage.setItem("Swipes", localStorage.getItem("Swipes") - 1);
+    }
+    if (localStorage.getItem("UsingSwipes") == null) {
+        localStorage.setItem("UsingSwipes",true);
+    }
 });

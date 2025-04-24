6a53b5295d24deb18d2d2a8c642a1ee709e1d16b
Korimer <thekclov@gmail.com>
2025-04-24 16:36:57 -0600

    base progress

--- assets/js/blocklinks.js
+++ assets/js/blocklinks.js
@@ -6,31 +6,46 @@ function coverLinks() {
       const rect = link.getBoundingClientRect();
 
       const cover = document.createElement('div');
-      cover.className = 'link-cover';
-      cover.style.top = `${rect.top + window.scrollY}px`;
-      cover.style.left = `${rect.left + window.scrollX}px`;
-      cover.style.width = `${rect.width}px`;
-      cover.style.height = `${rect.height}px`;
+      covertemplate(cover,rect);
 
+      cover.addEventListener('mouseover', () => {console.log("hi!")});
+      
       document.body.appendChild(cover);
+      cover.appendChild(document.createElement('div'));
     }
-    if (localStorage.getItem("swipesremaining") == 0) {
-        setInterval(coverLinks, 1);
+    if (parseInt(localStorage.getItem("swipesremaining")) <= 0) {
+        setTimeout(coverLinks, 1);
     }
     else {
       document.querySelectorAll('.link-cover').forEach(el => el.remove());
+      document.querySelectorAll("popup").forEach(el => el.remove());
     }
   }
+function updateCovers(covers) {
+  for (let i=0; i<covers.length; i++) {
+    const rect = document.links.item(i).getBoundingClientRect();
+    const cover = covers.item(i);
+    covertemplate(cover,rect);
+  } 
+}
+
+function covertemplate(cover,rect) {
+  cover.className = 'link-cover';
+  cover.style.top = `${rect.top + window.scrollY}px`;
+  cover.style.left = `${rect.left + window.scrollX}px`;
+  cover.style.width = `${rect.width}px`;
+  cover.style.height = `${rect.height}px`;
+}
 
 window.addEventListener('DOMContentLoaded', () => {
   let swipes = localStorage.getItem("swipesremaining");
   if (swipes == null || swipes == NaN || swipes == "NaN") {localStorage.setItem("swipesremaining",3)}
   localStorage.setItem("swipesremaining",localStorage.getItem("swipesremaining")-1)
-  if (localStorage.getItem("swipesremaining") == 0) {
+  if (parseInt(localStorage.getItem("swipesremaining")) <= 0) {
       coverLinks();
       setTimeout(() => {
-        localStorage.setItem("swipesremaining",2)
-      }, 1000);
+        localStorage.setItem("swipesremaining",2);
+      }, 10000);
   }
 });
 
